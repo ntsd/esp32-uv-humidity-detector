a4000688e0b41ae072ce5c34fc752349f0b1e3f1
ntsd <jo06942@gmail.com>
2020-03-26 02:15:10 +0700

    Update voltage to uv index

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 // Variables
 byte temperature = 0;
 byte humidity = 0;
-int uvValue = 0;
+int uvIndex = 0;
 
 // Pins
 int uvDetectorPin = A0;
@@ -44,7 +44,7 @@ void setup()
   Serial.println(WiFi.localIP());
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     AsyncResponseStream *response = request->beginResponseStream("text/html");
-    response->printf("<label>UV value: %d</label><br>", uvValue);
+    response->printf("<label>UV Index: %d</label><br>", uvIndex);
     response->printf("<label>Temperature: %d C</label><br>", (int) temperature);
     response->printf("<label>Humidity: %d%% RH</label><br>", (int) humidity);
     request->send(response);
@@ -58,9 +58,36 @@ void loop()
   Serial.println("=================================");
 
   // UV Detector
-  uvValue = analogRead(uvDetectorPin);
-  Serial.print("UV Value: ");
-  Serial.println(uvValue);
+  int sensorValue = analogRead(uvDetectorPin);
+  int voltage = (sensorValue * (5.0 / 1023.0)) * 1000; // Voltage in miliVolts
+  
+  if (voltage > 0 && voltage <= 227) {
+    uvIndex = 0;
+  } else if (voltage > 227 && voltage <= 318) {
+    uvIndex = 1;
+  } else if (voltage >318 && voltage <= 408) {
+    uvIndex = 2;
+  } else if (voltage > 408 && voltage <= 503) {
+    uvIndex = 3;
+  } else if (voltage > 503 && voltage <= 606) {
+    uvIndex = 4;
+  } else if (voltage > 606 && voltage <= 696) {
+    uvIndex = 5;
+  } else if (voltage > 696 && voltage <= 795) {
+    uvIndex = 6;
+  } else if (voltage > 795 && voltage <= 881) {
+    uvIndex = 7;
+  } else if (voltage > 881 && voltage <= 976) {
+    uvIndex = 8;
+  } else if (voltage > 976 && voltage <= 1079) {
+    uvIndex = 9;
+  } else if (voltage > 1079 && voltage <= 1170) {
+    uvIndex = 10;
+  } else if (voltage > 1170) {
+    uvIndex = 11;
+  }
+  Serial.print("UV Index: ");
+  Serial.println(uvIndex);
 
   // Huminity and Temperature DHT22
   int err = SimpleDHTErrSuccess;
