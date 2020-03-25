3cb76f5d65c073864325416a1ed86f41956a81d3
ntsd <jo06942@gmail.com>
2020-03-26 01:39:26 +0700

    Add esp8266, uv detector and dht22

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include "Arduino.h"
+#include "SimpleDHT.h"
+
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+
+// Variables
+byte temperature = 0;
+byte humidity = 0;
+int uvValue = 0;
+
+// Pins
+int uvDetectorPin = A0;
+int dht22Pin = 2;
+SimpleDHT22 dht22(dht22Pin);
+
+// Wifi
+AsyncWebServer server(80);
+const char* ssid = "ZONG MBB-E8372-95F5";
+const char* password = "41473716";
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  // Wifi setup
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.printf("WiFi Failed!\n");
+      return;
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    AsyncResponseStream *response = request->beginResponseStream("text/html");
+    response->print("UV Value: ");
+    response->printf((char*) uvValue);
+    response->print("Temperature: ");
+    response->print((char*) (int) temperature);
+    response->println(" C");
+    response->print("Humidity: ");
+    response->print((char*) (int) humidity);
+    response->println("% RH");
+    request->send(response);
+  });
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void loop() 
+{
+  Serial.println("=================================");
+
+  // UV Detector
+  uvValue = analogRead(uvDetectorPin);
+  Serial.print("UV Value: ");
+  Serial.println(uvValue);
+
+  // Huminity and Temperature DHT22
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht22.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT22 failed, err=");
+    Serial.println(err);
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print((int) temperature); Serial.println(" C");
+
+  Serial.print("Humidity: ");
+  Serial.print((int) humidity); Serial.println("% RH");
+
+  delay(2500);
+}
